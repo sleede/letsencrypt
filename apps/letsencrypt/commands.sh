000deb3eb32d7cd0404a57dec5985787e0d0dfd0
Sylvain <sylvain@sleede.com>
2025-02-11 13:59:10 +0100

    fix: removecert command

--- apps/letsencrypt/commands.sh
+++ apps/letsencrypt/commands.sh
@@ -1,7 +1,7 @@
 alias certificates='/usr/bin/docker run --rm --name certbot -v "/apps/letsencrypt/etc:/etc/letsencrypt" certbot/certbot:latest certificates'
 
 removecert() {
-  /usr/bin/docker run --rm --name certbot -v "/apps/letsencrypt/etc:/etc/letsencrypt" certbot/certbot:latest delete --cert-name "$1"
+  /usr/bin/docker run -it --rm --name certbot -v "/apps/letsencrypt/etc:/etc/letsencrypt" certbot/certbot:latest delete --cert-name "$1"
 }
 
 mkcert() {
